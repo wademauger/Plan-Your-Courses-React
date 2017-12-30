df37cff892a87df34e6b389afe793d987397cf04
Wade Mauger <wadeanthony0100@gmail.com>
2017-12-29 23:47:53 -0500

    Add SE as dev seed

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -6,6 +6,8 @@ import { CourseModalState } from './models/CourseModalState';
 import { serialize, deserialize } from 'serializr';
 import { saveAs } from 'file-saver';
 
+import { SE } from '../seed/SE';
+
 export class Store {
 
   @observable
@@ -22,57 +24,7 @@ export class Store {
     this.courseModalState = new CourseModalState(this.mainPlan);
 
     // Seed state, for dev only
-    this.mainPlan.addYear('First Year');
-    this.mainPlan.years[0].addTerm();
-    this.mainPlan.years[0].addTerm('Spring');
-
-    this.mainPlan.years[0].terms[0].addCourse('Software Engineering Seminar', 'SWEN', '101', 1);
-    this.mainPlan.years[0].terms[0].addCourse('Computer Science One', 'CSCI', '141', 4);
-    this.mainPlan.years[0].terms[0].addCourse('Project Based Calculus One', 'MATH', '141', 4);
-
-    this.mainPlan.years[0].terms[1].addCourse('Intro to SE', 'SWEN', '261', 3);
-    this.mainPlan.years[0].terms[1].addCourse('Computer Science Two', 'CSCI', '142', 4);
-    this.mainPlan.years[0].terms[1].addCourse('Project Based Calculus Two', 'MATH', '142', 4);
-
-    this.mainPlan.addYear('Second Year');
-    this.mainPlan.years[1].addTerm();
-    this.mainPlan.years[1].addTerm('Spring');
-
-    this.mainPlan.years[1].terms[0].addCourse('Software Engineering Seminar', 'SWEN', '101', 1);
-    this.mainPlan.years[1].terms[0].addCourse('Computer Science One', 'CSCI', '141', 4);
-    this.mainPlan.years[1].terms[0].addCourse('Project Based Calculus One', 'MATH', '141', 4);
-
-    this.mainPlan.years[1].terms[1].addCourse('Intro to SE', 'SWEN', '261', 3);
-    this.mainPlan.years[1].terms[1].addCourse('Computer Science Two', 'CSCI', '142', 4);
-    this.mainPlan.years[1].terms[1].addCourse('Project Based Calculus Two', 'MATH', '142', 4);
-
-    this.mainPlan.addYear('Third Year');
-    this.mainPlan.years[2].addTerm();
-    this.mainPlan.years[2].addTerm('Spring');
-
-    this.mainPlan.years[2].terms[0].addCourse('Software Engineering Seminar', 'SWEN', '101', 1);
-    this.mainPlan.years[2].terms[0].addCourse('Computer Science One', 'CSCI', '141', 4);
-    this.mainPlan.years[2].terms[0].addCourse('Project Based Calculus One', 'MATH', '141', 4);
-
-    this.mainPlan.years[2].terms[1].addCourse('Intro to SE', 'SWEN', '261', 3);
-    this.mainPlan.years[2].terms[1].addCourse('Computer Science Two', 'CSCI', '142', 4);
-    this.mainPlan.years[2].terms[1].addCourse('Project Based Calculus Two', 'MATH', '142', 4);
-
-    this.mainPlan.addYear('Fourth Year');
-    this.mainPlan.years[3].addTerm();
-    this.mainPlan.years[3].addTerm('Spring');
-
-    this.mainPlan.years[3].terms[0].addCourse('Software Engineering Seminar', 'SWEN', '101', 1);
-    this.mainPlan.years[3].terms[0].addCourse('Computer Science One', 'CSCI', '141', 4);
-    this.mainPlan.years[3].terms[0].addCourse('Project Based Calculus One', 'MATH', '141', 4);
-
-    this.mainPlan.years[3].terms[1].addCourse('Intro to SE', 'SWEN', '261', 3);
-    this.mainPlan.years[3].terms[1].addCourse('Computer Science Two', 'CSCI', '142', 4);
-    this.mainPlan.years[3].terms[1].addCourse('Project Based Calculus Two', 'MATH', '142', 4);
-
-    this.mainPlan.addColor('SWEN', 'blue');
-    this.mainPlan.addColor('CSCI', 'green');
-    this.mainPlan.addColor('MATH', 'red');
+    this.loadJSON(SE);
   }
 
   handleFileDrop([file]) {
