48052200e02ce5dfc3f1f8ac3dbb473499504626
Wade Mauger <wadeanthony0100@gmail.com>
2017-12-31 14:56:13 -0500

    Fix enzyme tests

--- src/components/__tests__/Course.test.jsx
+++ src/components/__tests__/Course.test.jsx
@@ -4,19 +4,28 @@ import { CourseModel } from '../../stores/models/CourseModel';
 import { Store } from '../../stores/Store.js';
 import { shallow, mount, render } from 'enzyme';
 
+// Course fails to shallow render if it's not
+// in the context of a droppable
 jest.mock('react-beautiful-dnd', () => ({
-  droppable: ({ children }) => (<div>
-    {children}
-  </div>),
+  Draggable: (props) => (
+    <div>
+      { props.children({dragHandleProps:{}}, {isDragging: false}) }
+    </div>
+  ),
+  Droppable: ({ children }) => (
+    <div>
+      { props.children({dragHandleProps:{}}, {isDragging: false}) }
+    </div>
+  ),
 }));
 
 describe('<Course />', () => {
-	it('Should render with a default Course Model', () => {
+  it('Should render with a default Course Model', () => {
 
-		const myCourseModel = new CourseModel('SW Architectures & Requirements', 'SWEN', '444', 3);
+    const myCourseModel = new CourseModel('SW Architectures & Requirements', 'SWEN', '444', 3);
     const myStore = new Store();
 
-		const wrapper = shallow(<Course course={myCourseModel} store={myStore} />);
+    const wrapper = mount(<Course course={myCourseModel} store={myStore} />);
     expect(wrapper.find('.header-text').text()).toBe(`${myCourseModel.dept} ${myCourseModel.num}`);
-	});
+  });
 });
