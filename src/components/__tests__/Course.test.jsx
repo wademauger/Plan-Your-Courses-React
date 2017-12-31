6558296c8674a9b69376f6594d4359e3e733f753
Wade Mauger <wadeanthony0100@gmail.com>
2017-12-31 12:48:50 -0500

    Upgrade Jest

---
+++ src/components/__tests__/Course.test.jsx
@@ -0,0 +1,15 @@
+import { Course } from '../Course';
+import { CourseModel } from '../../stores/models/CourseModel';
+import { Store } from '../../stores/Store.js';
+import { shallow, mount, render } from 'enzyme';
+
+describe('<Course />' () => {
+	it('Should render with a default Course Model', () => {
+
+		const myCourseModel = new CourseModel('SW Architectures & Requirements', 'SWEN', '444', 3);
+    const myStore = new Store();
+
+		const wrapper = shallow(<Course course={myCourseModel} store={myStore} />);
+    expect(wrapper.find('.header-text').text()).toBe(`${myCourseModel.dept} ${myCourseModel.num}`);
+	});
+});
