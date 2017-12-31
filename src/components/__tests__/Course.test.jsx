7d850d002733067cc3d985058ae8486e890d1723
Wade Mauger <wadeanthony0100@gmail.com>
2017-12-31 14:21:39 -0500

    Add tests, move mocks and stubs around

--- src/components/__tests__/Course.test.jsx
+++ src/components/__tests__/Course.test.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import { Course } from '../Course';
 import { CourseModel } from '../../stores/models/CourseModel';
 import { Store } from '../../stores/Store.js';
 import { shallow, mount, render } from 'enzyme';
 
-describe('<Course />' () => {
+jest.mock('react-beautiful-dnd', () => ({
+  droppable: ({ children }) => (<div>
+    {children}
+  </div>),
+}));
+
+describe('<Course />', () => {
 	it('Should render with a default Course Model', () => {
 
 		const myCourseModel = new CourseModel('SW Architectures & Requirements', 'SWEN', '444', 3);
--- src/stores/Store.js
+++ src/stores/Store.js
@@ -6,8 +6,6 @@ import { CourseModalState } from './models/CourseModalState';
 import { serialize, deserialize } from 'serializr';
 import { saveAs } from 'file-saver';
 
-import { SE } from '../seed/SE';
-
 export class Store {
 
   @observable
@@ -22,9 +20,6 @@ export class Store {
   constructor() {
     this.mainPlan = new PlanModel();
     this.courseModalState = new CourseModalState(this.mainPlan);
-
-    // Seed state, for dev only
-    this.loadJSON(SE);
   }
 
   handleFileDrop([file]) {
@@ -40,6 +35,11 @@ export class Store {
     const planJson = serialize(this.mainPlan);
     const file = new File([JSON.stringify(planJson)], `myPlan${Date.now()}.json`, {type: 'text/plain;charset=utf-8'});
     saveAs(file);
+  }
+
+  @action.bound seed(seedObject) {
+    // FOR DEVELOPMENT ONLY
+    this.loadJSON(seedObject);
   }
 
   @action.bound toggleSaveDropdown() {
