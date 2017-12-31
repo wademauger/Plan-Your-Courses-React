397219d7dd0244ecba8eae2bad5db1a7675a6ba1
Wade Mauger <wadeanthony0100@gmail.com>
2017-12-31 15:03:52 -0500

    Test <Course />

--- src/components/__tests__/Course.test.jsx
+++ src/components/__tests__/Course.test.jsx
@@ -5,27 +5,29 @@ import { Store } from '../../stores/Store.js';
 import { shallow, mount, render } from 'enzyme';
 
 // Course fails to shallow render if it's not
-// in the context of a droppable
+// in the context of a droppable, so we mock 
+// Draggable to blindly render its children
 jest.mock('react-beautiful-dnd', () => ({
-  Draggable: (props) => (
+  Draggable: ({ children }) => (
     <div>
-      { props.children({dragHandleProps:{}}, {isDragging: false}) }
+      { children({dragHandleProps:{}}, {isDragging: false}) }
     </div>
   ),
   Droppable: ({ children }) => (
     <div>
-      { props.children({dragHandleProps:{}}, {isDragging: false}) }
+      { children({dragHandleProps:{}}, {isDragging: false}) }
     </div>
   ),
 }));
 
 describe('<Course />', () => {
   it('Should render with a default Course Model', () => {
-
     const myCourseModel = new CourseModel('SW Architectures & Requirements', 'SWEN', '444', 3);
     const myStore = new Store();
-
     const wrapper = mount(<Course course={myCourseModel} store={myStore} />);
+
     expect(wrapper.find('.header-text').text()).toBe(`${myCourseModel.dept} ${myCourseModel.num}`);
+    expect(wrapper.find('.name').text()).toBe(myCourseModel.name);
+    expect(wrapper.find('.credits').text()).toBe(`[${myCourseModel.credits}]`);
   });
 });
